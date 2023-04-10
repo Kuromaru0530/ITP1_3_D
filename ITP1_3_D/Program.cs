865ab1081546d16af3f6b5d0f96a40f77ef82d91
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 15:55:43 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_3_D/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ITP1_3_D
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Numbers = Console.ReadLine();
+            string[] Blank = Numbers.Split(' ');
+
+            int a = int.Parse(Blank[0]);
+            int b = int.Parse(Blank[1]);
+            int c = int.Parse(Blank[2]);
+            int Count = 0;
+
+            do
+            {
+
+            } while (Count != b);
+        }
+    }
+}
